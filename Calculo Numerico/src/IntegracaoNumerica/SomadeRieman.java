bbc6c610dbb12c8ce7c755b58d6ef18904a5d2c8
João Sales <joaosales911@gmail.com>
2023-05-16 18:15:58 -0300

    Integração Numerica

--- Calculo Numerico/src/IntegracaoNumerica/SomadeRieman.java
+++ Calculo Numerico/src/IntegracaoNumerica/SomadeRieman.java
@@ -1,54 +1,23 @@
 package IntegracaoNumerica;
 
-import java.util.function.Function;
 //1) achar a integral pela soma de Rieman
 //dividr a area em varios retangulos e somar
 // soma de infinitos retangulos
-/// calcular pela esquerda (altura a esquerda) ou pela direita (altura a direita)
 public class SomadeRieman {
-
-    public static double riemannSum(Function<Double, Double> f, double a, double b, int n, String method) {
-        double dx = (b - a) / n;
-        double[] x = new double[n];
-        for (int i = 0; i < n; i++) {
-            x[i] = a + i * dx;
-        }
-        double[] points;
-        if (method.equals("esquerda")) {
-            points = new double[n - 1];
-            for (int i = 0; i < n - 1; i++) {
-                points[i] = x[i];
-            }
-        } else if (method.equals("direita")) {
-            points = new double[n - 1];
-            for (int i = 0; i < n - 1; i++) {
-                points[i] = x[i + 1];
-            }
-        } else if (method.equals("meio")) {
-            points = new double[n - 1];
-            for (int i = 0; i < n - 1; i++) {
-                points[i] = (x[i] + x[i + 1]) / 2;
-            }
-        } else {
-            throw new IllegalArgumentException("Método inválido: escolha 'direita', 'esquerda' ou 'meio'.");
-        }
-        double sum = 0;
-        for (double p : points) {
-            sum += f.apply(p) * dx;
-        }
-        return sum;
-    }
-
-    // Exemplo de uso
     public static void main(String[] args) {
-    	//formula:
-        Function<Double, Double> f = x -> x * x;
-        //limites de integração: 
-        double a = 6.48;
-        double b = 11.88;
-        int n = 100;
-        String method = "esquerda";
-        System.out.println("Soma de Riemann (" + method + ") para f(x)=x^2 no intervalo [" + a + "," + b + "]:");
-        System.out.println(riemannSum(f, a, b, n, method));
+    	 double[] valoresx = {2.0, 2.2, 2.4, 2.6, 2.8, 3.0};
+         double[] valoresy = {4.5, 6.8, 10.2, 11.2, 15.8, 20.0};
+
+         int n = valoresx.length - 1; // partições
+         double deltax = (valoresx[n] - valoresx[0]) / n;
+
+         double somaRieman = 0.0;
+         for (int i = 0; i < n; i++) {
+             double x = valoresx[i]; 
+             double fx = valoresy[i]; 
+             somaRieman += fx * deltax;
+         }
+
+         System.out.println("Valor da integral usando a soma de Rieman " + somaRieman);
     }
 }
