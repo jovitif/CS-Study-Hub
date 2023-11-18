d9fff62c39df7b68b464c621d088e81b31ad30a8
João Sales <joaosales911@gmail.com>
2023-11-18 02:54:49 -0300

    starting leetcode project

---
+++ LeetCode/src/ConcatenationofArray.java
@@ -0,0 +1,12 @@
+
+public class ConcatenationofArray {
+	
+	 public int[] getConcatenation(int[] nums) {
+	        return nums;
+	    }
+	 
+	 public static void main(String []args) {
+		 
+	 }
+
+}
